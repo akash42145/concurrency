11900093f82e5d957a4676f4ffd9ce77044102ff
Akash Sagar <akash14111985@gmail.com>
2020-08-13 20:30:24 +0530

    Rx java

---
+++ src/main/java/com/example/rxjava/ErrorHandledPriceApiClient.java
@@ -0,0 +1,22 @@
+package com.example.rxjava;
+
+import io.reactivex.Flowable;
+
+import java.util.concurrent.TimeUnit;
+
+public class ErrorHandledPriceApiClient
+{
+    public static Flowable<String> pricesAtInterval()
+    {
+        return Flowable
+            .interval(100, TimeUnit.MILLISECONDS)
+            .map(x -> PriceApiService.getPriceLimited())
+            /*.retry()*/;
+    }
+
+    public static void main(String[] args)
+    {
+        pricesAtInterval()
+            .blockingSubscribe(System.out::println, Throwable::printStackTrace);
+    }
+}
---
+++ src/main/java/com/example/rxjava/synchronous/service/DelayMXBean.java
@@ -0,0 +1,8 @@
+package com.example.rxjava.synchronous.service;
+
+public interface DelayMXBean
+{
+    long getDelayInMs();
+
+    void setDelayInMs(final long delay);
+}
